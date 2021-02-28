af1d46bb9d5ca48039df4ee369a32eb7fd69d0a9
mollnn <1084918073@qq.com>
2021-02-28 08:37:41 +0800

    HelloQt

---
+++ HelloQt/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    QLabel *label=new QLabel("Hello");
+    label->show();
+    return a.exec();
+}
